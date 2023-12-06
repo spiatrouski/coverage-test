0cc09fb2b9bd3b88ff712f022e6f641bb8fe9c04
Siarhei Piatrouski <siarhei.piatrouski95@gmail.com>
2023-12-06 16:29:53 +0200

    init commit

---
+++ src/main/java/com/example/coveragetest/service/MathService.java
@@ -0,0 +1,11 @@
+package com.example.coveragetest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MathService {
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/test/java/com/example/coveragetest/MathServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.coveragetest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.coveragetest.service.MathService;
+
+@ExtendWith(MockitoExtension.class)
+class MathServiceTest {
+
+    @InjectMocks
+    private MathService mathService;
+
+    @Test
+    void sub() {
+        assertThat(mathService.sub(4, 3)).isEqualTo(1);
+    }
+
+}
