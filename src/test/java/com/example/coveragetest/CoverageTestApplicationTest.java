285f7a96f82b32d54d5092836fba48584acf44aa
Siarhei Piatrouski <siarhei.piatrouski95@gmail.com>
2023-12-07 17:51:36 +0200

    add public static void main test

---
+++ src/test/java/com/example/coveragetest/CoverageTestApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.coveragetest;
+
+import org.junit.jupiter.api.Test;
+
+class CoverageTestApplicationTest {
+
+    @Test
+    void testMain() {
+        CoverageTestApplication.main(new String[0]);
+    }
+}
